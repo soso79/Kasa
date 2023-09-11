2dcba7307c4912dd26e912584b32d4b4f07a4a7a
sofiane bahri <bahrisofiane79@gmail.com>
2023-09-11 18:11:57 +0200

    creation router components et page et style

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const componentName = (props) => {
+    return (
+        <div>
+            <h1>Acceuil</h1>
+        </div>
+    );
+};
+
+export default componentName;
