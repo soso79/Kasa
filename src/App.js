65d6ae39b9d1bb3f183d7043b14ffb22c75d9345
sofiane bahri <bahrisofiane79@gmail.com>
2023-09-13 11:25:40 +0200

    essai youtube

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const App = (props) => {
   return (
-    <h1>y en a marre</h1>
+    <h1>Hello react</h1>
   );
 };
 
