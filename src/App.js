93f7eae9265b4c541b6f26bd39b3bbdbdf6b7c05
sofiane bahri <bahrisofiane79@gmail.com>
2023-09-17 15:45:51 +0200

    Ajout routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import A Propos from "./pages/A Propos";
+import Fiche-logement from "./pages/Fiche-logement";
+import 404 from "./pages/404";
+
 const App = (props) => {
   return (
-    <h1>Hello react</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/A propos" element={<A Propos />} />
+        <Route path="/Fiche-logement" element={<Fiche-logement />} />
+        <Route path="*" element={< 404 />} />
+      </Routes>
+
+
+    </BrowserRouter>
   );
 };
 
