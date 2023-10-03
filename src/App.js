fd29591e6070869658fb23693daffd2e1291572e
sofiane bahri <bahrisofiane79@gmail.com>
2023-10-03 18:38:14 +0200

    modification structure creation dossier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import About from "./pages/About/About";
 import Error from "./pages/Error";
 import Logements from "./pages/Logements";
 
---
+++ src/pages/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../../components/Navigation';
+import Header from '../../components/Header';
+
+const About = (props) => {
+    return (
+        <div className='About-can'>
+            <Header />
+            <div>
+
+            </div>
+            <h1>A propos</h1>
+        </div>
+    );
+};
+
+export default About;
