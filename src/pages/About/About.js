3b7a9c471194d5427c13d555a270faa8436b19da
sofiane bahri <bahrisofiane79@gmail.com>
2023-10-08 18:26:10 +0200

    creation collapse avec descripstion et titre

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,10 @@ const About = (props) => {
     return (
         <div className='About-can'>
             <img className='Banner-About' alt='Paysage' src={Photo} />
-
+            <Collapse titre="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
+            <Collapse titre="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+            <Collapse titre="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
+            <Collapse titre="Sécurité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
         </div>
     );
 };
