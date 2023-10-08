386fa1aaec65f761a969cfe5eb4a6637a04791fd
sofiane bahri <bahrisofiane79@gmail.com>
2023-10-08 16:07:11 +0200

    page error.js fini

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import '../styles/Error.css'
-import Header from '../../components/Header';
-import photo from '../assets/404.png'
-import Footer from '../../components/Footer';
+import '../styles/Error.css';
+import { NavLink } from "react-router-dom";
+
 
 const Error = (props) => {
     return (
 
         <div className='Error-Can'>
-            <Header />
-            <div className='Test'>
-                <img src={photo} alt='page erreur' className='Error' />
-                <p className='Error-Text'>Oups! La page que vous demandez n existe pas.</p>
-            </div>
-            <Footer />
+
+
+            <h1 className='ErrorTitle'>404</h1>
+            <p className='Error-Text'>Oups! La page que vous demandez n existe pas.</p>
+            <NavLink to="/" className="lien-site lien-404">Retourner sur la page d’accueil</NavLink>
+
+
 
         </div>
 
