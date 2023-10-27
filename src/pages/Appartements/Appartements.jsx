9db51772ce401c3ebb5a689d5afe3dcd514b3bfa
sofiane bahri <bahrisofiane79@gmail.com>
2023-10-27 17:15:55 +0200

    brouillon appartement en bonne voix

--- src/pages/Appartements/Appartements.jsx
+++ src/pages/Appartements/Appartements.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import LogementsData from "../../assets/logements.json";
-
+import './Appartements.scss'
+import Header from "../../components/Header/Header"
 const Logements = () => {
     const { id } = useParams();
     const logement = LogementsData.find((logement) => logement.id === id);
@@ -12,10 +13,33 @@ const Logements = () => {
 
     return (
         <div className="Logements">
-            <h2>{logement.title}</h2>
-            <img src={logement.cover} alt={logement.title} />
-            <p>{logement.description}</p>
-            {/* Affichez ici d'autres détails du logement */}
+            <Header />
+            <div className="Logements-img">
+                <img className="appart" src={logement.cover} alt={logement.title} />
+            </div>
+            <div className="Logements-can">
+                <div className="Logements-tag">
+                    <h2>{logement.title}</h2>
+                    <p>{logement.location}</p>
+                    <p>{logement.tags}</p>
+                </div>
+                <div className="Logements-rate">
+
+                    <p> {logement.host.name}
+                        <img className="Rate-picture" src={logement.host.picture} alt={logement.host.name} /></p>
+                    <p>{logement.rating}</p>
+                </div>
+
+            </div>
+            <p>Équipements :</p>
+            <ul>
+                {logement.equipments.map((equipement, index) => (
+                    <li key={index}>{equipement}</li>
+                ))}
+            </ul>
+            <p> Description:{logement.description}</p>
+
+
         </div>
     );
 };
