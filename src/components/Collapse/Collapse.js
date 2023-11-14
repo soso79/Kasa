e7e79b9278e29dcb84773de9e1aee8d23531e403
sofiane bahri <bahrisofiane79@gmail.com>
2023-11-15 00:29:47 +0100

    projet fini avec up collapse

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Collapse.css';
 import ArrowDown from '../../assets/ArrowDown.png';
+import ArrowUp from '../../assets/ArrowUp.png'
 
 function Collapse({ data }) {
     // État local pour suivre les indices des sections ouvertes
@@ -8,25 +9,21 @@ function Collapse({ data }) {
 
     // Fonction pour basculer l'état d'ouverture/fermeture d'une section
     const toggleCollapse = (index) => {
-        if (openIndexes.includes(index)) {
-            // Si la section est déjà ouverte, la fermer en retirant son index de openIndexes
-            setOpenIndexes(openIndexes.filter((i) => i !== index));
-        } else {
-            // Si la section est fermée, l'ouvrir en ajoutant son index à openIndexes
-            setOpenIndexes([...openIndexes, index]);
-        }
+        setOpenIndexes(openIndexes.includes(index)
+            ? openIndexes.filter((i) => i !== index) // Si ouvert, le retirer
+            : [...openIndexes, index] // Si fermé, l'ajouter
+        );
     };
 
     return (
-        // Conteneur principal avec une classe CSS conditionnelle en fonction de la longueur de data
         <div className={`CollapseHome${data.length === 2 ? 'Two' : ''}`}>
             {data.map((collapse, index) => (
                 <div className="ContCollapse" key={index}>
                     <div className="ContTittle" onClick={() => toggleCollapse(index)}>
                         <h3 className="CollapseTittle">{collapse.title}</h3>
-                        <img src={ArrowDown} alt="Arrow" />
+                        {/* Changer l'icône en fonction de l'état d'ouverture */}
+                        <img src={openIndexes.includes(index) ? ArrowUp : ArrowDown} alt="Arrow" />
                     </div>
-
                     {/* Affiche le texte de la section uniquement si elle est ouverte */}
                     {openIndexes.includes(index) && (
                         <div className="CollapseText">{collapse.text}</div>
