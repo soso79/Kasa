c4e72bfde8f84b64cfc5d8ff34ad3c868672386a
sofiane bahri <bahrisofiane79@gmail.com>
2023-11-16 17:08:10 +0100

    ajout useState et useEffect

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 import Logements from '../../components/Logements/Logements';
 import LogementsData from '../../assets/logements.json';
 import { NavLink } from 'react-router-dom';
 
 const Card = (props) => {
+    // État local pour suivre la carte de logement actuellement cliquée
+    const [selectedLogement, setSelectedLogement] = useState(null);
+
+    // Fonction pour gérer le clic sur une carte de logement
+    const handleCardClick = (logement) => {
+        setSelectedLogement(logement);
+        // Vous pouvez effectuer d'autres actions en réponse au clic ici
+    };
+
     return (
         <div className='appartement-can'>
             <div className='appartement-card'>
                 {LogementsData.map((logement) => (
                     <NavLink to={`/logement/${logement.id}`} key={logement.id}>
-
-                        <Logements
+                        <div
+                            className={`card ${logement === selectedLogement ? 'selected' : ''}`}
                             key={logement.id}
-                            id={logement.id}
-                            title={logement.title}
-                            cover={logement.cover}
-
-
-                        />
-
+                            onClick={() => handleCardClick(logement)} // Gestionnaire d'événements de clic
+                        >
+                            <Logements
+                                key={logement.id}
+                                id={logement.id}
+                                title={logement.title}
+                                cover={logement.cover}
+                            />
+                        </div>
                     </NavLink>
                 ))}
-
             </div>
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../../assets/img/LOGO.png';
-import './Header.css'
+import './Header.css';
 import { NavLink } from 'react-router-dom';
+
 const Header = (props) => {
+    // État pour suivre quel lien a été cliqué
+    const [selectedLink, setSelectedLink] = useState(null);
+
+    // Fonction pour gérer le clic sur un lien
+    const handleLinkClick = (link) => {
+        setSelectedLink(link);
+    };
+
+    // useEffect pour effectuer des actions en réponse au clic sur les liens
+    useEffect(() => {
+        if (selectedLink === 'accueil') {
+            // Code à exécuter lorsque le lien "Accueil" est cliqué
+            console.log('Le lien "Accueil" a été cliqué.');
+            // Vous pouvez effectuer d'autres actions en réponse au clic ici
+        } else if (selectedLink === 'apropos') {
+            // Code à exécuter lorsque le lien "A propos" est cliqué
+            console.log('Le lien "A propos" a été cliqué.');
+            // Vous pouvez effectuer d'autres actions en réponse au clic ici
+        }
+    }, [selectedLink]); // L'effet se déclenche lorsque selectedLink change
+
     return (
         <header className='head'>
             <NavLink to="/">
@@ -11,16 +33,21 @@ const Header = (props) => {
                 </div>
             </NavLink>
             <div className='test'>
-                <NavLink to="/" className="head-nav-link">
+                <NavLink
+                    to="/"
+                    className={`head-nav-link ${selectedLink === 'accueil' ? 'selected' : ''}`}
+                    onClick={() => handleLinkClick('accueil')}
+                >
                     <div>Accueil</div>
                 </NavLink>
-                <NavLink to="/about" className="head-nav-link">
+                <NavLink
+                    to="/about"
+                    className={`head-nav-link ${selectedLink === 'apropos' ? 'selected' : ''}`}
+                    onClick={() => handleLinkClick('apropos')}
+                >
                     <div>A propos</div>
                 </NavLink>
             </div>
-
-
-
         </header>
     );
 }
