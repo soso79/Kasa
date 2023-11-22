bae52f9fbbc55053c6105ceb7e98677c23a4a0ff
sofiane bahri <bahrisofiane79@gmail.com>
2023-11-22 15:47:14 +0100

    petite corection font size et body

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -10,27 +10,27 @@ const Slider = () => {
     const { id } = useParams();
     const logement = LogementsData.find((logement) => logement.id === id);
 
-
-
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     const handleNextImage = () => {
         setCurrentImageIndex((prevIndex) =>
             prevIndex === logement.pictures.length - 1 ? 0 : prevIndex + 1
         );
+        console.log("Clic sur la flèche suivante");
     };
 
     const handlePreviousImage = () => {
         setCurrentImageIndex((prevIndex) =>
             prevIndex === 0 ? logement.pictures.length - 1 : prevIndex - 1
         );
+        console.log("Clic sur la flèche précédente");
     };
 
-    return (
-        <div className="Slider">
-            {/* ... Autres contenus du composant Logements */}
-            <div className="Slider__Can">
-                <img className="Slider__Img" src={logement.pictures[currentImageIndex]} alt="Logement" />
+    // Utilisez une instruction if pour décider d'afficher ou non les flèches et le compteur
+    let navigationAndCounter = null;
+    if (logement.pictures.length > 1) {
+        navigationAndCounter = (
+            <>
                 <img
                     className="Slider__Prev__Arrow"
                     src={ArrowRight}
@@ -44,6 +44,20 @@ const Slider = () => {
                     onClick={handleNextImage}
                 />
                 <p className="Slider__Compteur"> {currentImageIndex + 1}/{logement.pictures.length}</p>
+            </>
+        );
+    }
+
+    return (
+        <div className="Slider">
+            {/* ... Autres contenus du composant Logements */}
+            <div className="Slider__Can">
+                <img
+                    className="Slider__Img"
+                    src={logement.pictures[currentImageIndex]}
+                    alt="Logement"
+                />
+                {navigationAndCounter}
             </div>
         </div>
     );
