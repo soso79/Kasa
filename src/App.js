a62bb993cfcaf67f0fde1064611962cc1e580f0d
sofiane bahri <bahrisofiane79@gmail.com>
2023-11-25 05:45:47 +0100

    create router properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx";
-import About from "./pages/About/About.jsx";
-import Error from "./pages/Error/Error.jsx";
-import Appartements from "./pages/Appartements/Appartements.jsx";
-
+import Router from "./Router";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/logement/:id" element={<Appartements />} />
-
-      </Routes>
-
-
-    </BrowserRouter>
+    <div>
+      <Router />
+    </div>
   );
-};
-
+}
 export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About/About.jsx";
+import Error from "./pages/Error/Error.jsx";
+import Appartements from "./pages/Appartements/Appartements.jsx";
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />
+                <Route path="/logement/:id" element={<Appartements />} />
+
+            </Routes>
+
+
+        </BrowserRouter>
+    );
+};
+
+export default Router;
